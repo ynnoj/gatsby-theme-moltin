f4c32acaf7ef59c837f33a5e391156c9259c0be6
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-07-05 12:24:53 +0100

    refactor: useStaticQuery hook

--- packages/gatsby-theme-moltin/src/pages/index.js
+++ packages/gatsby-theme-moltin/src/pages/index.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import ProductGrid from '../components/ProductGrid'
 
-const IndexPage = ({ data: { products } }) => (
-  <ProductGrid products={products.edges.map(({ node: product }) => product)} />
-)
-
-export const query = graphql`
-  query {
-    products: allMoltinProduct {
-      edges {
-        node {
-          id
-          name
-          slug
-          path
+const IndexPage = () => {
+  const { products } = useStaticQuery(graphql`
+    query {
+      products: allMoltinProduct {
+        edges {
+          node {
+            id
+            name
+            slug
+            path
+          }
         }
       }
     }
-  }
-`
+  `)
+
+  return (
+    <ProductGrid
+      products={products.edges.map(({ node: product }) => product)}
+    />
+  )
+}
 
 export default IndexPage
